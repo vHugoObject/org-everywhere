b588c383047ee973fadc27f85e312d846f597da6
vHugoObject <echase3@gmail.com>
2025-07-22 05:19:55 -0500

    First try

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import { githubPagesSpa } from '@sctg/vite-plugin-github-pages-spa';
 // https://vite.dev/config/
 export default defineConfig({
   base: "org-everywhere",
