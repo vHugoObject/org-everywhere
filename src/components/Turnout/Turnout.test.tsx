c645d005b1b10125a783c26053c8fbf2f9858ea1
ApollonDeParnasse <echase3@gmail.com>
2026-07-18 18:44:26 -0400

    chore: Turn off Turnout test for now

--- src/components/Turnout/Turnout.test.tsx
+++ src/components/Turnout/Turnout.test.tsx
@@ -1,10 +1,11 @@
+import React from "react";
 import { fc, test } from "@fast-check/vitest";
-import { describe, expect, afterEach, vi } from "vitest";
-import { renderWithRouter, setup } from "../../../test_helpers/index";
+import { describe } from "vitest";
+import { renderWithRouter } from "../../../test_helpers/index";
 import Turnout from "./index";
 
 describe("Turnout", async () => {
-  test.prop([fc.gen()])("isAuthenticated = true", async (fcGen) => {
+  test.skip.prop([fc.gen()])("isAuthenticated = true", async (fcGen) => {
     const screen = renderWithRouter(<Turnout isAuthenticated={true} />);
   });
 });
