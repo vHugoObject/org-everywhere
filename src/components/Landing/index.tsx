f215dc784053ce4bf8fc183f76e150cc3c21c74e
ApollonDeParnasse <echase3@gmail.com>
2026-07-31 14:24:45 -0400

    chore: Repetition

--- src/components/Landing/index.tsx
+++ src/components/Landing/index.tsx
@@ -9,15 +9,6 @@ export default () => {
     <>
       <nav className="navbar navbar-marketing navbar-expand-lg bg-transparent navbar-dark fixed-top navbar-scrolled">
         <div className="container px-5">
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          ></button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <a
               className="btn fw-500 ms-lg-4 btn-teal"
@@ -34,7 +25,7 @@ export default () => {
       <div className="row gx-5 align-items-center">
         <div className="col-lg-6" data-aos="fade-up">
           <h1 className="page-header-ui-title">
-            org-everywhere is org everywhere
+            org-everywhere
           </h1>
           <p>You get the point</p>
 
